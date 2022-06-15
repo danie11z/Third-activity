c08cf87cd9a05b75212daff8eae8ff64bd65f027
danie11z <daniel.estudando1@gmail.com>
2022-06-15 08:09:07 -0300

    3rd activity JS

---
+++ script.js
@@ -0,0 +1,21 @@
+for(let count = 1; count <= 10; count += 1){
+    console.log(count)
+}
+
+let countTwo = 10
+
+while(1 <= countTwo){
+    console.log(countTwo)
+    countTwo -= 1
+}
+
+let countThree = 1
+
+while(100 > countThree){
+    console.log(countThree)
+    countThree += 2
+}
+
+for(let countFour = 0; countFour <= 100; countFour += 2){
+    console.log(countFour)
+}
